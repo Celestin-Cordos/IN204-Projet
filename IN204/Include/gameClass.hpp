7e00cc830230322de292e6ea23505a4e52a9aad5
Celestin-Cordos <63093619+Celestin-Cordos@users.noreply.github.com>
2020-12-15 18:33:27 +0100

    beginning classes
    Board et Pieces

--- IN204/Include/gameClass.hpp
+++ IN204/Include/gameClass.hpp
@@ -1,12 +1,16 @@
+
 #ifndef GAMECLASS_HPP
 #define GAMECLASS_HPP
 
 #include <iostream>
 #include <vector>
-#include "gameClass.hpp"
+//#include "gameClass.hpp"
+
+#include <SFML/Graphics.hpp>
+
 using namespace std;
 
-enum color {Nan, red, green, bleue, yellow, pink, purple};
+enum color {Black, Red, Green, Bleue, Yellow, Pink, Purple}; // Black est une case vide
 enum type {I, O, T, L, J, Z, S};
 enum direction {Left, Right};
 
@@ -17,24 +21,35 @@ public:
 	type piece_type;
 	color piece_color;
 	//list of positions (cases) it's filling in the matrix
-	vector< tuple<int, int> > posi_arr;
 
 	Piece();
-	Piece(type piece_type);
+	Piece(type typeP, color colorP);
 	~Piece();
 
 	void rotate(unsigned int deg);
 	void move(direction dir);
 };
 
-class Game
+class Board
 {
-public:
-	Case board[20][10];
+    int pas; // taille d'une case (carrée)
+    int HEIGHT; // en nombre de cases (pour pas voir de probleme d'arrondi en cas de divisions...)
+    int WIDTH; // en nombre de cases (pour pas voir de probleme d'arrondi en cas de divisions...)
+    color* MATRIX;//matrice de cases
 
-	Game();
-	~Game();
+    Board (int step, int hauteur, int largeur);
+
+
 };
+
+//class Game
+//{
+//public:
+//	Case board[20][10];
+//
+//	Game();
+//	~Game();
+//};
 
 
 #endif
--- IN204/Sources/gameClass.cpp
+++ IN204/Sources/gameClass.cpp
@@ -1,5 +1,31 @@
 #include <iostream>
 #include "../include/gameClass.hpp"
+#include <SFML/Graphics.hpp>
 
 using namespace std;
+
+Piece::Piece(type typeP, color colorP)
+{
+    piece_type = typeP;
+    piece_color = colorP;
+}
+
+Board::Board (int pas, int hauteur, int largeur)
+{
+    STEP = pas;
+    HEIGHT = hauteur;
+    WIDTH = largeur;
+    color matrice [largeur*hauteur];
+    for (int i = 0; i<hauteur*largeur; i++ )
+    {
+        matrice[i] = Black;
+    }
+    MATRIX=matrice;
+}
+
+Board::~Board()
+{
+    delete MATRIX;
+    std::cout << "Board deleted succesflully" << std::endl;
+}
 
