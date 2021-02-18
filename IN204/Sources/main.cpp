21b7d4e9fa319a2a0ab9af8ce28d36fdd07c3fc4
Guillaume Duceux <guillaume.duceux@cuisinous.com>
2021-02-18 17:49:29 +0100

    add main function

--- IN204/Sources/main.cpp
+++ IN204/Sources/main.cpp
@@ -149,3 +149,30 @@ void MainClient() {
     window.display();
   }
 }
+
+int main(int argc, char *argv[]) {
+  std::cout << "Bienvenue dans le test de communication de Tetris" << std::endl;
+
+  char who;
+  std::cout << "Do you want to be a server (s) or a client (c) ? ";
+  std::cin >> who;
+
+  if (who == 's') {
+    std::cout << "Creation d'un serveur" << std::endl;
+    std::string password = " ";
+    // ask for password to login as server
+    while (password != "tetris") {
+      std::cout << "Enter password : ";
+      std::cin >> password;
+      if (password != "tetris") {
+        std::cout << "Wrong password! Please try again.\n" << std::endl;
+      }
+    }
+
+    MainServeur();
+    return 0;
+  }
+
+  MainClient();
+  return 0;
+}
